5fd359074284568ac5a60331804b4eb2a9482fe6
MichelThomsen <miph89@hotmail.com>
2026-07-01 14:27:50 +0200

    Tilfoej service worker

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'fjallraven-v1';
+const SHELL = ['./index.html', './Fjallraven_Classic_Danmark_2026.kml', './icon.svg', './manifest.json'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL).catch(() => {})));
+});
+
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // Lad Firebase og CDN-kald gå direkte på nettet
+  if (url.origin !== location.origin) return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
