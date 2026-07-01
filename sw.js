048a38aa89ab60f0e0b3b083db80d6e12af22ad4
MichelThomsen <miph89@hotmail.com>
2026-07-01 16:18:36 +0200

    SW v4: slet gamle caches i activate

--- sw.js
+++ sw.js
@@ -1,16 +1,19 @@
-const CACHE = 'fjallraven-v3';
-const SHELL = ['./index.html', './Fjallraven_Classic_Danmark_2026.kml', './icon.svg', './manifest.json'];
+const CACHE = 'fjallraven-v4';
+const SHELL = ['./index.html', './Fjallraven_Classic_Danmark_2026.kml', './icon.svg', './manifest.json', './manifest-checkpoint.json'];
 
 self.addEventListener('install', e => {
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL).catch(() => {})));
 });
 
-self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys()
+    .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => clients.claim())
+));
 
 self.addEventListener('fetch', e => {
   const url = new URL(e.request.url);
-  // Lad Firebase og CDN-kald gå direkte på nettet
   if (url.origin !== location.origin) return;
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request).then(res => {
